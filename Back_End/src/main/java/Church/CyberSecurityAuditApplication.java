2797312512e9a604f01db288dc49ab54e06d47be
TheChupa <160878340+TheChupa@users.noreply.github.com>
2025-07-21 11:06:20 -0500

    initial setup

---
+++ Back_End/src/main/java/Church/CyberSecurityAuditApplication.java
@@ -0,0 +1,13 @@
+package Church;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSecurityAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberSecurityAuditApplication.class, args);
+	}
+
+}
